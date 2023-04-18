00d40fd8a70d82e64e67bd6e0aefb5422b9e7e3f
Nazso <zsolt.nagy.1976@gmail.com>
2023-04-18 16:02:48 +0200

    pract

--- 03-number-math/pract_HR.js
+++ 03-number-math/pract_HR.js
@@ -1,9 +1,16 @@
-let n = 0;
-let x = 0;
+// let n = 0;
+// let x = 0;
 
-while (n < 3) {
-  n++;
-  x += n;
-}
+// while (n < 3) {
+//   n++;
+//   x += n;
+// }
 
-console.log(x)
+// console.log(x);
+
+let a = 2;
+let b = 4;
+
+a *= a + b * a / 2;
+
+console.log(a);
