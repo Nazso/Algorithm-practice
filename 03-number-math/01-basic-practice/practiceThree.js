13aa0bda0955c3944b321b4191297bf453deac7f
Nazso <zsolt.nagy.1976@gmail.com>
2023-05-19 11:37:36 +0200

    pract

--- 03-number-math/01-basic-practice/practiceThree.js
+++ 03-number-math/01-basic-practice/practiceThree.js
@@ -163,8 +163,8 @@
 
   function maxMin(k, arr) {
     
-    const sortedArr = arr.sort((a, b) => a - b)
-    const newArr = [];
+    // const sortedArr = arr.sort((a, b) => a - b)
+    // const newArr = [];
     // let smallest = Infinity;
 
     // sortedArr.forEach((e, i) => {
@@ -194,4 +194,4 @@
 
 }
 
-console.log(maxMin(5, [4504, 1520, 5857, 4094, 4157, 3902, 822, 6643, 2422, 7288, 8245, 9948, 2822, 1784, 7802, 3142, 9739, 5629, 5413, 7232]));
+// console.log(maxMin(5, [4504, 1520, 5857, 4094, 4157, 3902, 822, 6643, 2422, 7288, 8245, 9948, 2822, 1784, 7802, 3142, 9739, 5629, 5413, 7232]));
